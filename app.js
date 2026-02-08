4d396088dde9968ac5bd42c589a6bd485caa7054
dakshumeena <dakshumeena698@gmail.com>
2026-02-09 00:13:49 +0530

    env cloudinary

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ if(process.env.NODE_ENV !=="production"){
   require('dotenv').config()
 }
 console.log(process.env.CLOUDINARY_SECRET)
+console.log(process.env.CLOUDINARY_KEY);
+
 
 const express=require("express");
 const app=express();
